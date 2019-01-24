3d94b0b2a7746811d8f6242a1bc3095e35c9daaf
Scott Barnes <tearabite@gmail.com>
2019-01-24 17:57:55 -0600

    Add documentation to queue class.

--- queue.js
+++ queue.js
@@ -3,32 +3,51 @@ class Queue {
         this.elements = [];
     }
 
+    /**
+     * Adds a new element to the end of the queue.
+     * @param {*} element The element to add to the queue.
+     */
     enqueue (element) {
         this.elements.push(element);
     }
 
+    /**
+     * Removes the element next in the queue and returns it.
+     */
     dequeue () {
         if (!this.isEmpty) {
             return this.elements.shift();
         }
     }
 
+    /**
+     * Returns the element next in the queue, but does not modify the queue.
+     */
     peek () {
         if (!this.elements.isEmpty) {
             return this.elements[0];
         }
     }
 
+    /**
+     * The number of elements in the queue.
+     */
     get size () {
         return this.elements.length;
     }
 
-    print () {
-        console.log(`${this.elements.join(', ')}`)
-    }
-
+    /**
+     * Returns whether there are any elements in the queue.
+     */
     get isEmpty() {
         return this.size === 0;
+    }
+
+    /**
+     * Prints the queue to the console.
+     */
+    print () {
+        console.log(`${this.elements.join(', ')}`)
     }
 }
 
