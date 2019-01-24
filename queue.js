474ecb1b44abb5e52181d5844fd2bb6e40181fa8
Scott Barnes <tearabite@gmail.com>
2019-01-24 17:32:39 -0600

    Add basic di-graph and queue classes.

---
+++ queue.js
@@ -0,0 +1,36 @@
+module.exports = {
+    Queue:
+    class Queue {
+        constructor() {
+            this.elements = [];
+        }
+
+        enqueue (element) {
+            this.elements.push(element);
+        }
+
+        dequeue () {
+            if (!this.isEmpty) {
+                return this.elements.shift();
+            }
+        }
+
+        peek () {
+            if (!this.elements.isEmpty) {
+                return this.elements[0];
+            }
+        }
+
+        get size () {
+            return this.elements.length;
+        }
+
+        print () {
+            console.log(`${this.elements.join(', ')}`)
+        }
+
+        get isEmpty() {
+            return this.size === 0;
+        }
+    }
+}
